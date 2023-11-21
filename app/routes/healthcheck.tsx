df7bee13f01747f66d0ce27070660cfad86e93b1
Chris Berger <flippinberger@gmail.com>
2023-11-21 14:06:50 -0700

    Update health check message to test deploy

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -19,7 +19,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     ]);
     return new Response("OK");
   } catch (error: unknown) {
-    console.log("healthcheck ❌", { error });
+    console.log("healthcheck failed ❌", { error });
     return new Response("ERROR", { status: 500 });
   }
 };
